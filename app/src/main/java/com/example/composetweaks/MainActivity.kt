b24237936273f0a605c0e504e5414199abbe2d00
Kashif Masood <khanzada.kashif2001@gmail.com>
2023-07-12 01:02:43 +0500

    Added rating bar to show floating point rating. Code clean up.

--- app/src/main/java/com/example/composetweaks/MainActivity.kt
+++ app/src/main/java/com/example/composetweaks/MainActivity.kt
@@ -8,6 +8,10 @@ import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.composetweaks.ui.theme.ComposeTweaksTheme
@@ -17,6 +21,10 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTweaksTheme {
+                var rating by remember {
+                    mutableStateOf(0f)
+                }
+
                 LazyColumn(
                     modifier = Modifier.fillMaxSize(),
                     verticalArrangement = Arrangement.spacedBy(32.dp),
@@ -26,10 +34,18 @@ class MainActivity : ComponentActivity() {
                         OTPView(numberOfFields = 4){}
                     }
                     item {
-                        FilledSlider {}
+                        FilledSlider(
+                            rangeFrom = 0f,
+                            rangeTo = 5f
+                        ) {
+                            rating = it
+                        }
                     }
                     item {
-                        RatingStarInFloat()
+                        RatingStarInFloat(
+                            rating = rating,
+                            maxRating = 5
+                        )
                     }
                 }
             }
