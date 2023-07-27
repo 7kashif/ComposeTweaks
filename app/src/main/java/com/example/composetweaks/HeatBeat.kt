a7cce6bae120fe28e6e29cc3b3f73d0e4cca8055
Kashif Masood <khanzada.kashif2001@gmail.com>
2023-07-27 23:22:14 +0500

    Achieved animation stillness after reaching mid screen.

--- app/src/main/java/com/example/composetweaks/HeatBeat.kt
+++ app/src/main/java/com/example/composetweaks/HeatBeat.kt
@@ -2,6 +2,7 @@ package com.example.composetweaks
 
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.offset
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
@@ -9,9 +10,11 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.unit.dp
 import com.example.composetweaks.ui.theme.Blue
 import kotlinx.coroutines.delay
 import kotlin.math.PI
@@ -26,29 +29,49 @@ fun HeartBeat() {
         mutableStateOf(0f)
     }
 
+    var start by remember {
+        mutableStateOf(0f)
+    }
+
+    var startMovingBack by remember {
+        mutableStateOf(false)
+    }
+
 
     LaunchedEffect(Unit) {
         delay(50)
-        for (i in 0..widthInFloat.toInt()) {
+        for (i in 0..Int.MAX_VALUE) {
             xCord = i.toFloat()
             delay(10)
+            if (i == widthInFloat.toInt() / 2)
+                startMovingBack = true
+        }
+    }
+
+    LaunchedEffect(key1 = startMovingBack) {
+        while (startMovingBack) {
+            start -= 2f
+            delay(60)
         }
     }
 
     Canvas(
         modifier = Modifier
             .fillMaxSize()
+            .offset(x = start.dp)
             .graphicsLayer {
                 widthInFloat = size.width
             }
     ) {
+        val canvasWidth = size.width
         val canvasHeight = size.height
 
         val wavePath = Path()
         wavePath.moveTo(0f, canvasHeight / 2)
 
-        val amplitude = 50f
-        val frequency = 0.01f
+
+        val amplitude = 80f
+        val frequency = 0.015f
 
         for (x in 0..xCord.toInt()) {
             val y = canvasHeight / 2 + amplitude * cos(2 * PI * x * frequency)
@@ -56,5 +79,12 @@ fun HeartBeat() {
         }
 
         drawPath(wavePath, color = Blue, style = Stroke(6f))
+
+        drawLine(
+            color = Blue,
+            start = Offset(0f, canvasHeight / 2),
+            end = Offset(canvasWidth + xCord, canvasHeight / 2),
+            strokeWidth = 4f
+        )
     }
 }
