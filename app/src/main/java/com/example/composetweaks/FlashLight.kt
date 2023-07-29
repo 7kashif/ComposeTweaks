83dd2fae2796bc52763218463dcd334987795b61
Kashif Masood <khanzada.kashif2001@gmail.com>
2023-07-29 21:12:47 +0500

    Created flash light kind of reading experience.

---
+++ app/src/main/java/com/example/composetweaks/FlashLight.kt
@@ -0,0 +1,72 @@
+package com.example.composetweaks
+
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.gestures.detectTapGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun FlashLight() {
+
+    var center by remember {
+        mutableStateOf(Offset(-300f,-300f))
+    }
+
+    val radius by remember {
+        mutableStateOf(300f)
+    }
+
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Black),
+        contentAlignment = Alignment.Center
+    ) {
+        Canvas(
+            modifier = Modifier
+                .fillMaxSize()
+                .pointerInput(Unit) {
+                    detectDragGestures { change, _ ->
+                        center = change.position
+                    }
+                },
+            onDraw = {
+                drawCircle(
+                    brush = Brush.radialGradient(
+                        colors = listOf(Color.White, Color.Transparent),
+                        center = center,
+                        radius = radius
+                    ),
+                    radius = radius,
+                    center = center
+                )
+
+            }
+        )
+        Text(
+            text = stringResource(id = R.string.random_text),
+            style = TextStyle(
+                color = Color.Black,
+                fontSize = 24.sp
+            )
+        )
+    }
+
+}
--- app/src/main/java/com/example/composetweaks/MainActivity.kt
+++ app/src/main/java/com/example/composetweaks/MainActivity.kt
@@ -10,7 +10,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTweaksTheme {
-                HeartBeat()
+                FlashLight()
             }
         }
     }
