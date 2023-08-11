fd68677b4bec7fb1bdfe30543e73165ebf75bee7
Kashif Masood <khanzada.kashif2001@gmail.com>
2023-08-11 23:31:55 +0500

    Damping effect on circle.

--- app/src/main/java/com/example/composetweaks/PlayPauseButton.kt
+++ app/src/main/java/com/example/composetweaks/PlayPauseButton.kt
@@ -1,14 +1,15 @@
 package com.example.composetweaks
 
 import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animate
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.animateOffsetAsState
 import androidx.compose.animation.core.spring
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -20,13 +21,11 @@ import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeJoin
-import androidx.compose.ui.graphics.drawscope.DrawStyle
-import androidx.compose.ui.graphics.drawscope.Fill
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.graphics.drawscope.rotate
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
-import com.example.composetweaks.ui.theme.Blue
+import kotlinx.coroutines.delay
 
 private const val DELAY = 1000
 
@@ -42,6 +41,23 @@ fun PlayPauseButton() {
     var isPaused by remember {
         mutableStateOf(true)
     }
+    var animateRadius by remember {
+        mutableStateOf(false)
+    }
+
+    val circleRadius by animateFloatAsState(
+        targetValue = if (animateRadius) 210f else 220f,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioHighBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
+    LaunchedEffect(key1 = isPaused, block = {
+        animateRadius = true
+        delay((DELAY / 2).toLong())
+        animateRadius = false
+    })
 
     val playAlpha by animateFloatAsState(
         targetValue = if (isPaused) 0f else 1f,
@@ -59,7 +75,7 @@ fun PlayPauseButton() {
     )
 
     val pauseRotation by animateFloatAsState(
-        targetValue = if (isPaused) 0f else 40f,
+        targetValue = if (isPaused) 0f else 30f,
         animationSpec = tween(DELAY)
     )
 
@@ -84,23 +100,13 @@ fun PlayPauseButton() {
 
             drawCircle(
                 center = Offset(hw, hh),
-                radius = 220f,
+                radius = circleRadius,
                 style = Stroke(width = 8.dp.toPx()),
                 color = Color.Black
             )
 
-
-
-            drawRoundRect(
-                color = Color.Black,
-                topLeft = Offset((hw - 100f) - pauseRotation, (hh - 120f) - pauseRotation),
-                cornerRadius = CornerRadius(12f.dp.toPx(), 12f.dp.toPx()),
-                size = Size(30.dp.toPx(), 90f.dp.toPx()),
-                alpha = pauseAlpha
-            )
-
             rotate(
-                degrees = 30f
+                degrees = playRotation
             ) {
                 drawPath(
                     path = path,
@@ -110,13 +116,25 @@ fun PlayPauseButton() {
                 )
             }
 
-            drawRoundRect(
-                color = Color.Black,
-                topLeft = Offset((hw + 10) - pauseRotation, (hh - 120f) - pauseRotation),
-                cornerRadius = CornerRadius(12f.dp.toPx(), 12f.dp.toPx()),
-                size = Size(30.dp.toPx(), 90f.dp.toPx()),
-                alpha = pauseAlpha
-            )
+            rotate(
+                degrees = pauseRotation
+            ) {
+                drawRoundRect(
+                    color = Color.Black,
+                    topLeft = Offset((hw - 100f) - pauseRotation, (hh - 120f) - pauseRotation),
+                    cornerRadius = CornerRadius(12f.dp.toPx(), 12f.dp.toPx()),
+                    size = Size(30.dp.toPx(), 90f.dp.toPx()),
+                    alpha = pauseAlpha
+                )
+
+                drawRoundRect(
+                    color = Color.Black,
+                    topLeft = Offset(hw + 10, hh - 120f),
+                    cornerRadius = CornerRadius(12f.dp.toPx(), 12f.dp.toPx()),
+                    size = Size(30.dp.toPx(), 90f.dp.toPx()),
+                    alpha = pauseAlpha
+                )
+            }
         }
     )
 }
