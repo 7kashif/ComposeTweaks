ab2b972f4af4a4c52fc566efdbb753a3708ee4c5
Kashif Masood <khanzada.kashif2001@gmail.com>
2023-08-14 00:18:16 +0500

    Animated ratina in show/hide.

--- app/src/main/java/com/example/composetweaks/ShowHide.kt
+++ app/src/main/java/com/example/composetweaks/ShowHide.kt
@@ -3,6 +3,7 @@ package com.example.composetweaks
 import androidx.compose.animation.core.Spring
 import androidx.compose.animation.core.animateFloatAsState
 import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.layout.fillMaxSize
@@ -10,30 +11,56 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+private const val eyeDelay: Long = 500
 
 @Composable
 fun ShowHide() {
-
     var show by remember {
         mutableStateOf(false)
     }
+    val scope = rememberCoroutineScope()
+
+    var eyeOffSetX by remember {
+        mutableStateOf(0f)
+    }
 
     val animateCurve by animateFloatAsState(
-        targetValue = if (show) 80f else -80f,
+        targetValue = if (show) 100f else -100f,
         animationSpec = spring(
             dampingRatio = Spring.DampingRatioMediumBouncy,
             stiffness = Spring.StiffnessMedium
-        )
+        ),
+        finishedListener = {
+            if(it == 100f) {
+                scope.launch {
+                    eyeOffSetX = -120f
+                    delay(eyeDelay)
+                    eyeOffSetX = 120f
+                    delay(eyeDelay)
+                    eyeOffSetX = 0f
+                }
+            }
+        }
     )
 
+
+    val eyeOffSet by animateFloatAsState(
+        targetValue = eyeOffSetX,
+        animationSpec = tween(eyeDelay.toInt())
+    )
 
     Canvas(
         modifier = Modifier
@@ -56,22 +83,36 @@ fun ShowHide() {
                     x2 = hw + 100f,
                     y2 = hh - animateCurve
                 )
-                if(show) {
-                    cubicTo(
-                        x3 = hw - 200f,
-                        y3 = hh,
-                        x1 = hw - 100f,
-                        y1 = hh + 80f,
-                        x2 = hw + 100f,
-                        y2 = hh + 80f
-                    )
-                }
             }
+
+            val path2 = Path().apply {
+                moveTo(hw - 200f, hh)
+                cubicTo(
+                    x3 = hw + 200f,
+                    y3 = hh,
+                    x1 = hw - 100f,
+                    y1 = hh + 100f,
+                    x2 = hw + 100f,
+                    y2 = hh + 100f
+                )
+            }
+
+            drawCircle(
+                color = Color.Black,
+                center = Offset(hw + eyeOffSet,hh),
+                radius = 40f,
+                alpha = if(show) 1f else 0f,
+            )
 
             drawPath(
                 path = path,
                 color = Color.Black,
                 style = Stroke(width = 8.dp.toPx(), cap = StrokeCap.Round)
+            )
+            drawPath(
+                path = path2,
+                color = Color.Black,
+                style = Stroke(width = 8.dp.toPx(), cap = StrokeCap.Round),
             )
         }
     )
