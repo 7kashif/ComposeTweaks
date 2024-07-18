042ce05c75daab15c7bccfe6a4a1f292d12e04f5
Kashif Masood <khanzada.kashif2001@gmail.com>
2024-07-18 12:58:58 +0500

    Added a coupled scroller in lazy column.

--- app/src/main/java/com/example/composetweaks/MainActivity.kt
+++ app/src/main/java/com/example/composetweaks/MainActivity.kt
@@ -3,9 +3,14 @@ package com.example.composetweaks
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.composetweaks.ui.theme.ComposeTweaksTheme
 
 
@@ -14,7 +19,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ComposeTweaksTheme {
-               AnimatedTick()
+               CoupledScroller()
             }
         }
     }
