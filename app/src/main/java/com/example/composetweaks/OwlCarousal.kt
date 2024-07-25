f98e1d01bd993307d332194f35f51fddba0348e3
Kashif Masood <khanzada.kashif2001@gmail.com>
2024-07-25 11:44:46 +0500

    Optimised PercentageGauge.kt

--- app/src/main/java/com/example/composetweaks/OwlCarousal.kt
+++ app/src/main/java/com/example/composetweaks/OwlCarousal.kt
@@ -6,11 +6,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerState
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.unit.dp
@@ -26,12 +29,13 @@ fun OwlCarousal() {
         pageCount = Int.MAX_VALUE,
         modifier = Modifier.fillMaxWidth(),
         state = pagerState,
-        contentPadding = PaddingValues(horizontal = 48.dp)
+        contentPadding = PaddingValues(horizontal = 48.dp),
+        pageSpacing = 8.dp
     ) { page ->
         //defined in ClubbedPhotos.kt.
         PhotoItem(
             uri = listOfImages[page % listOfImages.size],
-            modifier = Modifier.carouselTransition(page, pagerState),
+            modifier = Modifier.clip(RoundedCornerShape(8.dp)).carouselTransition(page, pagerState),
             imageHeight = 200.dp
         )
     }
--- app/src/main/java/com/example/composetweaks/PercentageGauge.kt
+++ app/src/main/java/com/example/composetweaks/PercentageGauge.kt
@@ -1,24 +1,26 @@
 package com.example.composetweaks
 
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Slider
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.geometry.Size
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.Path
@@ -31,69 +33,67 @@ import com.example.composetweaks.ui.theme.GREY
 import com.example.composetweaks.ui.theme.Green
 import com.example.composetweaks.ui.theme.Orange
 import com.example.composetweaks.ui.theme.RED
+import kotlinx.coroutines.delay
 
 @Composable
 fun PercentageGauge(
-    modifier: Modifier = Modifier
+    modifier: Modifier = Modifier,
+    percentage: Float = 84f
 ) {
-    val (slideValue, updateSliderValue) = remember {
-        mutableStateOf(0f)
+    var slideValue by remember { mutableStateOf(0f) }
+    var animateVisibility by remember { mutableStateOf(false) }
+
+    val (status, innerArcColor) = remember(percentage) {
+        when (percentage) {
+            in 0f..50f -> "Warning" to RED
+            in 50f..65f -> "Low" to Orange
+            in 65f..80f -> "Good" to Green
+            else -> "Great!" to DarkGreen
+        }
     }
 
-    var status by remember {
-        mutableStateOf("Warning")
-    }
-
-    val innerArcColor by remember(slideValue) {
-        mutableStateOf(
-            when (slideValue) {
-                in 0f..50f -> {
-                    status = "Warning"
-                    RED
-                }
-
-                in 50f..65f -> {
-                    status = "Low"
-                    Orange
-                }
-
-                in 65f..80f -> {
-                    status = "Good"
-                    Green
-                }
-
-                else -> {
-                    status = "Great!"
-                    DarkGreen
-                }
-            }
+    //Outer arc segments
+    val arcSegments = remember {
+        mutableStateListOf(
+            50f to RED,
+            65f to Orange,
+            80f to Green,
+            100f to DarkGreen
         )
+    }
+
+    val animatedStatusAlpha by animateFloatAsState(
+        targetValue = if (animateVisibility) 1f else 0f,
+        animationSpec = tween(durationMillis = 1000), label = "animatedStatusAlpha"
+    )
+
+    LaunchedEffect(key1 = Unit) {
+        for (i in 0..percentage.toInt()) {
+            slideValue = i.toFloat()
+            delay(10)
+        }
+        animateVisibility = true
     }
 
     Box(
-        modifier = modifier.fillMaxSize(),
+        modifier = Modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        Slider(
-            value = slideValue,
-            onValueChange = updateSliderValue,
-            valueRange = 0f..100f,
-            modifier = Modifier
-                .align(Alignment.TopCenter)
-                .padding(32.dp),
-        )
-
-        Box {
+        Box(modifier = modifier) {
 
             Text(
                 text = "0",
                 style = MaterialTheme.typography.body1,
-                modifier = Modifier.align(Alignment.BottomStart).offset(x = -(56).dp)
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .offset(x = -(56).dp)
             )
             Text(
                 text = "100",
                 style = MaterialTheme.typography.body1,
-                modifier = Modifier.align(Alignment.BottomEnd).offset(x = 72.dp)
+                modifier = Modifier
+                    .align(Alignment.BottomEnd)
+                    .offset(x = 72.dp)
             )
             Column(
                 modifier = Modifier
@@ -103,7 +103,8 @@ fun PercentageGauge(
             ) {
                 Text(
                     text = status,
-                    style = MaterialTheme.typography.body1
+                    style = MaterialTheme.typography.body1,
+                    modifier = Modifier.alpha(animatedStatusAlpha)
                 )
                 Text(
                     text = "${slideValue.toInt()}%",
@@ -111,61 +112,12 @@ fun PercentageGauge(
                 )
             }
 
-            Canvas(
-                modifier = Modifier
-                    .size(260.dp, 130.dp)
-            ) {
+
+            Canvas(modifier = Modifier.size(200.dp, 100.dp)) {
                 val canvasWidth = size.width
                 val canvasHeight = size.height
 
-                val firstArc = Path().apply {
-                    arcTo(
-                        rect = Rect(
-                            offset = Offset(-85f, -90f),
-                            size = Size(canvasWidth + 170f, (canvasHeight * 2) + 180f)
-                        ),
-                        startAngleDegrees = -180f,
-                        sweepAngleDegrees = 50 * 1.8f,
-                        forceMoveTo = false
-                    )
-                }
-
-                val secondArc = Path().apply {
-                    arcTo(
-                        rect = Rect(
-                            offset = Offset(-85f, -90f),
-                            size = Size(canvasWidth + 170f, (canvasHeight * 2) + 180f)
-                        ),
-                        startAngleDegrees = -(180f - (50 * 1.8f)),
-                        sweepAngleDegrees = 15f * 1.8f,
-                        forceMoveTo = false
-                    )
-                }
-
-                val thirdArc = Path().apply {
-                    arcTo(
-                        rect = Rect(
-                            offset = Offset(-85f, -90f),
-                            size = Size(canvasWidth + 170f, (canvasHeight * 2) + 180f)
-                        ),
-                        startAngleDegrees = -(180f - (65 * 1.8f)),
-                        sweepAngleDegrees = 15f * 1.8f,
-                        forceMoveTo = false
-                    )
-                }
-                val fourthArc = Path().apply {
-                    arcTo(
-                        rect = Rect(
-                            offset = Offset(-85f, -90f),
-                            size = Size(canvasWidth + 170f, (canvasHeight * 2) + 180f)
-                        ),
-                        startAngleDegrees = -(180f - (80 * 1.8f)),
-                        sweepAngleDegrees = 20f * 1.8f,
-                        forceMoveTo = false
-                    )
-                }
-
-                //draw inner arc background
+                // Draw inner arc background
                 drawArc(
                     color = GREY,
                     startAngle = 0f,
@@ -175,7 +127,7 @@ fun PercentageGauge(
                     size = Size(canvasWidth, canvasHeight * 2)
                 )
 
-                //draw inner arc foreground
+                // Draw inner arc foreground
                 drawArc(
                     color = innerArcColor,
                     startAngle = -180f,
@@ -185,27 +137,18 @@ fun PercentageGauge(
                     size = Size(canvasWidth, canvasHeight * 2)
                 )
 
-                //draw outer segmented arc
-                drawPath(
-                    path = firstArc,
-                    color = RED,
-                    style = Stroke(width = 8.dp.toPx())
-                )
-                drawPath(
-                    path = secondArc,
-                    color = Orange,
-                    style = Stroke(width = 8.dp.toPx())
-                )
-                drawPath(
-                    path = thirdArc,
-                    color = Green,
-                    style = Stroke(width = 8.dp.toPx())
-                )
-                drawPath(
-                    path = fourthArc,
-                    color = DarkGreen,
-                    style = Stroke(width = 8.dp.toPx())
-                )
+                arcSegments.forEachIndexed { index, (start, color) ->
+                    val previousSegment = if (index == 0) 0f else arcSegments[index - 1].first
+                    drawArc(
+                        color = color,
+                        startAngle = -(180f - (previousSegment * 1.8f)),
+                        sweepAngle = (start - previousSegment) * 1.8f,
+                        useCenter = false,
+                        style = Stroke(width = 8.dp.toPx()),
+                        size = Size(canvasWidth + 170f, (canvasHeight * 2) + 180f),
+                        topLeft = Offset(-85f, -90f)
+                    )
+                }
 
                 rotate(
                     slideValue * 1.8f,
@@ -228,5 +171,6 @@ fun PercentageGauge(
             }
         }
     }
-}
 
+
+}
