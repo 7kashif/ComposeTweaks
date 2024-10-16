be8470aacb83153543f1718cbece3bee9b536adb
Kashif Masood <khanzada.kashif2001@gmail.com>
2024-10-16 11:24:02 +0500

    Moved AnimatedArrow animation from svg to shape.

--- app/src/main/java/com/example/composetweaks/AnimatingArrow.kt
+++ app/src/main/java/com/example/composetweaks/AnimatingArrow.kt
@@ -1,30 +1,32 @@
 package com.example.composetweaks
 
-import androidx.compose.animation.core.InfiniteRepeatableSpec
 import androidx.compose.animation.core.LinearEasing
-import androidx.compose.animation.core.StartOffset
-import androidx.compose.animation.core.StartOffsetType
-import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.offset
-import androidx.compose.material.Icon
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.isTraceInProgress
 import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Outline
+import androidx.compose.ui.graphics.Path
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.platform.LocalDensity
+import androidx.compose.ui.unit.Density
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.LayoutDirection
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.delay
 
@@ -63,21 +65,48 @@ fun AnimatingArrow() {
         }
     }
 
+    val density= LocalDensity.current
+
+    val strokePx = remember {
+        with(density) {
+            6.dp.toPx()
+        }
+    }
+
     Box(
         modifier = Modifier.fillMaxSize(),
         contentAlignment = androidx.compose.ui.Alignment.Center
     ) {
         Row {
             repeat(4) {
-                Icon(
-                    painter = painterResource(id = R.drawable.chevron_right),
-                    contentDescription = null,
+                Box(
                     modifier = Modifier
-                        .offset(x = if (it == 0) 0.dp else (-18 * it).dp, y = 0.dp)
+                        .offset(x = if (it == 0) 0.dp else (-3 * it).dp, y = 0.dp)
+                        .clip(ChevronShape(strokePx))
+                        .size(12.dp, 16.dp)
                         .alpha(arrowAlphas[it].value)
+                        .background(Color(0xFFb56f02))
                 )
             }
         }
     }
+}
+
+class ChevronShape(
+    private val strokeWidth: Float
+): Shape {
+    override fun createOutline(
+        size: Size,
+        layoutDirection: LayoutDirection,
+        density: Density
+    ): Outline = Outline.Generic(path = Path().apply {
+        moveTo(0f,0f)
+        lineTo(size.width - strokeWidth, size.height / 2)
+        lineTo(0f, size.height)
+        lineTo(0f + strokeWidth, size.height)
+        lineTo(size.width, size.height / 2)
+        lineTo(strokeWidth, 0f)
+        close()
+    })
 
 }
